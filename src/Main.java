24dd38b7484f221ca37a438e8ccb8a281a96d6fb
Glazov Nikita <niglazov@gmail.com>
2023-02-19 21:53:53 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket_price = 50000;
+        int one_mile = 20;
+
+
+        int miles = ticket_price / one_mile;
+        System.out.println("Вам начислено " + miles + " бонусных миль");
+    }
+}
